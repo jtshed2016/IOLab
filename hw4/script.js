af8b7b605420eefdf2dff4a25f361ae6451405d5
jtshed2016 <jshedlock@berkeley.edu>
2016-02-20 17:26:45 -0800

    Uploading HW4 after finally getting it working (after deadline).  The previous commit that was submitted before deadline should be graded, but I would be interested in feedback about this approach.

--- hw4/script.js
+++ hw4/script.js
@@ -1,44 +1,33 @@
-//test whether page is loading script
-console.log('Script loaded');
-
-//assigning lists from HTML to variables
-var toDoList = $('#todolist');
-var completeList = $('#donelist');
 
 //add new item to TO DO list from text field
 function newListItem() {
+	//get user-entered value from text field
 	var addedItem = $('#newlistitem').val();
-	$('#todolist').prepend("<li>" + addedItem +'</li><button onclick="moveToDone()">Done</button>');
-	//console.log(toDoList);
+	//add to TO DO list
+	$('#todolist').prepend("<li>" + addedItem +'</li><button onclick="moveToDone(this)">Done</button>');
+	//reset text field to blank
 	$('#newlistitem').val('');
 
 }
 
-//move item from ToDo list to done list...not working due to difficulty capturing current element
-//fail...
-function moveToDone() {
-	alert($(this).text);
-	//var thisitem = $("button").closest("li").text();
-	//thisitem=$("button").closest("li").val();
-	//var current = $(this).closest().innerHTML;
-	//alert(current)
-	//alert(current);
-	//$('#donelist').prepend(thisitem);
-	//alert(thisitem.text());
-	//console.log($("button").closest("li").html())
-	//$("#donelist").prepend("<li>" +thisitem.text() + '<button type="button" onclick="moveBackToDo()">Done</button></li>');
-	//thisitem.remove();
+//move item from ToDo list to done list
+function moveToDone(focusButton) {
+	//get list item corresponding to button
+	var thisitem = $(focusButton).prev();
+	//delete button and list item
+	$(focusButton).remove();
+	thisitem.remove();
+	//put button and HTML from to do list item into done list
+	$("#donelist").prepend('<button onclick="moveBackToDo(this)">Undo</button>');
+	$("#donelist").prepend('<li>' + thisitem.html() + '</li>');
 
 }
 //placeholder for moving things back to todo list
-function moveBackToDo (){
-	alert('Back!');
+function moveBackToDo (backButton){
+	var undoneItem = $(backButton).prev();
+	$(backButton).remove();
+	undoneItem.remove();
+	$("#todolist").prepend('<button onclick="moveToDone(this)">Done</button>');
+	$("#todolist").prepend('<li>' + undoneItem.html() + '</li>');
 }
 
-//former test function when I was trying to do this by arrays; no longer used
-function reciteAll() {
-	console.log('Reciting');
-	for (i=toDoList.length-1; i>=0; i--) {
-		alert(toDoList[i]);
-	}
-}
